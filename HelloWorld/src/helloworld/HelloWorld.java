be182206f842ff935b795abb6052bf2e848640c9
Zachary Murphy <murphyz@my.wccc.edu>
2016-10-26 17:17:19 -0400

    initial

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+
+package helloworld;
+
+/**
+ *
+ * @author Zachary Murphy
+ */
+public class HelloWorld {
+
+   //main method of program
+    public static void main(String[] args) {
+        //variable declarations
+        int myAge = 20;
+        int myBirthYear = 1996;
+        String myName = "Zachary Murphy";
+        
+        //outputs
+        System.out.println("Hello World!!!");
+        System.out.println("My name is " + myName + ", I am " + myAge + " years old.");
+        System.out.println("I was born in " + myBirthYear + ".");
+    }
+    
+}
